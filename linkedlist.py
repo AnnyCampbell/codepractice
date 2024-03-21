c39b6d863d7c4ef65fde1ac37db7b700220e7a04
Campbell <anny.campbell@sap.com>
2024-03-21 15:50:40 -0700

    add linkedlist

---
+++ linkedlist.py
@@ -0,0 +1,16 @@
+string1 = "apple"
+string2 = "banana"
+string3 = "matcha"
+string4 = "waffels"
+list1 = []
+list1.append(string4)
+list1.append(string3)
+list1.append(string2)
+list1.append(string1)
+print(list1)
+list1.sort()
+print(list1)
+#string3 ="zebra"
+list1.append(string3)
+print(list1)
+#study in how python handles linked lists 
